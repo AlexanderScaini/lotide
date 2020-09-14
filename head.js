c829e028a8c6da5368e9de53b12191278b43869e
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-14 19:14:49 +0000

    Created the head file that tests what the first entry in the array is

--- head.js
+++ head.js
@@ -6,9 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 4);
-assertEqual(true, true);
-// doesnt work thanks js!
-assertEqual(NaN, NaN);
+const head = function(array) {
+  return array.shift();
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([5]), 5);
