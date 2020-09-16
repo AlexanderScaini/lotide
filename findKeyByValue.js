df0e2c08136f036287cb3f083e3bf0aa6222229a
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-16 19:37:44 +0000

    created a function to search through an object and find the key by inputing its value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`✖️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearchThrough, valueToSearchWith) {
+  const values = Object.values(objectToSearchThrough);
+  const keys = Object.keys(objectToSearchThrough);
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === valueToSearchWith) {
+      return keys[i];
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
