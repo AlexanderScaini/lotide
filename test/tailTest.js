0025d2b1e585db51bb3b880ebbede2a1b248aa24
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-21 18:49:42 +0000

    converted the test cases for middle and tail to mocha chai.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,12 @@
+const { assert } = require("chai");
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail", () => {
+  it("returned ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+});
