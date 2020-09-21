ad95ac487d3f3b12d99c909eadf246e5170089e6
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-21 17:22:54 +0000

    refactored eqArrays and seperated the test cases into their own file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    // console.log(`✔️ Assertion Passed: ${actual}  === ${expected}`);
-    return true;
-  } else {
-    // console.log(`✖️ Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-};
-
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
-    if (!assertEqual(firstArray[i], secondArray[i])) {
-      return console.log(false);
+    if (!(firstArray[i] === secondArray[i])) {
+      return false;
     }
   }
-  return console.log(true);
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
